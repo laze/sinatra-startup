b7aef3f6db4c310318f09ac2d4d76a6c538579d3
Balázs Ercsey <laze@laze.hu>
2014-09-03 18:18:05 +0200

    Added layouts and partial templates.

--- app/main.rb
+++ app/main.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
-# require 'sinatra/reloader' if development?
+require 'sinatra/reloader'
 # require 'mongoid'
 # require 'slim'
 require 'erb'
@@ -7,8 +7,17 @@ require 'redcarpet'
  
 configure do
   # Mongoid.load!("./mongoid.yml")
+  # Set your Google Analytics ID here if you have one:
+    set :google_analytics_id, 'UA-12345678-1'
+
+    set :layouts_dir, './app/views/layouts'
+    set :partials_dir, './app/views/partials'
 end
 
 get '/' do
-	erb :index
+    @page_title = 'Hello World!'
+
+    erb :index,
+	   :layout => :default,
+       :layout_options => {:views => settings.layouts_dir}
 end
