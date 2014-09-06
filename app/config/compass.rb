d4b7a79ca7952650737205c03a61b137fc45db95
Ercsey Balázs <laze@laze.hu>
2014-09-06 23:32:45 +0200

    Compass configuration

--- app/config/compass.rb
+++ app/config/compass.rb
@@ -1,13 +1,13 @@
 require "bootstrap-sass"
 
 if defined?(Sinatra)
-  environment     = :development
+  environment = :development
 
-  project_path  = Sinatra::Application.root
+  project_path = Sinatra::Application.root
 else
-  environment     = :production
+  environment = :production
 
-  project_path  = File.dirname(__FILE__)
+  project_path = File.dirname(__FILE__)
 
   relative_assets = true
 end
@@ -15,10 +15,10 @@ end
 #** Set up directories.
 #* @see http://compass-style.org/help/documentation/sass-based-configuration-options/
 #**
-  css_dir       = File.join("public", "stylesheets")
-  images_dir    = File.join("public", "images")
-  fonts_dir     = File.join("public", "fonts")
-sass_dir                = File.join("views", "sass")
-http_path               = "/"
-http_images_path        = "/images"
-http_stylesheets_path   = "/stylesheets"
+css_dir = File.join("public", "stylesheets")
+images_dir = File.join("public", "images")
+fonts_dir = File.join("public", "fonts")
+sass_dir = File.join("views", "sass")
+http_path = "/"
+http_images_path = "/images"
+http_stylesheets_path = "/stylesheets"
