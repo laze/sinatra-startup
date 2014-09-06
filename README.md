04f9505b52e1cd3cfb484205d1a2de64c13ed73f
Ercsey Balázs <laze@laze.hu>
2014-09-07 00:14:53 +0200

    #2 fixed Add configuration for compass. Changer header files to get ready for the compiled compass. Added some plus to README.
    Add Bootstrap Sass compiled files.

--- README.md
+++ README.md
@@ -3,6 +3,20 @@ Sinatra Start
 
 Start your Sinatra project easily like a... I don't know. Just do it!
 
+# Getting Started
+
+Fetch repository.
+
+run bundle install
+
+bootstrap-sass precompiled
+
+# Usage
+Modify sass
+
+cd app
+run compass compile
+
 
 <blockquote class="twitter-tweet" lang="en"><p>I just published “Sinatra prunella” <a href="https://t.co/H7tbljTbee">https://t.co/H7tbljTbee</a></p>&mdash; laze (@laze) <a href="https://twitter.com/laze/status/508041487578185728">September 5, 2014</a></blockquote>
 <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
--- app/config/compass.rb
+++ app/config/compass.rb
@@ -7,7 +7,7 @@ if defined?(Sinatra)
 else
   environment = :production
 
-  project_path = File.dirname(__FILE__)
+  project_path = File.dirname('../')
 
   relative_assets = true
 end
@@ -15,10 +15,12 @@ end
 #** Set up directories.
 #* @see http://compass-style.org/help/documentation/sass-based-configuration-options/
 #**
-css_dir = File.join("public", "stylesheets")
-images_dir = File.join("public", "images")
-fonts_dir = File.join("public", "fonts")
-sass_dir = File.join("views", "sass")
+css_dir = File.join(project_path, "public", "stylesheets")
+images_dir = File.join(project_path, "public", "images")
+fonts_dir = File.join(project_path, "public", "fonts")
+sass_dir = File.join(project_path, "views", "sass")
+javascripts_dir = File.join(project_path, "public", "javascripts")
 http_path = "/"
 http_images_path = "/images"
 http_stylesheets_path = "/stylesheets"
+http_javascripts_path = "/javascripts"
