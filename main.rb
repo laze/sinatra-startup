c830ddc7cf6b756e4157d27e1d0133a19c2970e6
Ercsey Balázs <laze@laze.hu>
2014-09-13 00:44:11 +0200

    #4 Code cleaned up, 500 error page fixed.

--- main.rb
+++ main.rb
@@ -13,49 +13,42 @@ class Main < Sinatra::Application
     disable :show_exceptions
     disable :raise_errors
 
-     #** Set up Sinatra variables
-    set :app_file,      __FILE__
-    set :root,          File.dirname(__FILE__)
-    set :views,         File.join(File.dirname(__FILE__), 'views')
-    set :public_dir,    File.join(File.dirname(__FILE__), 'public')
-    set :layouts_dir,   File.join(File.dirname(__FILE__), 'views', 'layouts')
-    set :partials_dir,  File.join(File.dirname(__FILE__), 'views', 'partials')
+    #** Set up Sinatra variables
+    set :app_file, __FILE__
+    set :root, File.dirname(__FILE__)
+    set :views, File.join(File.dirname(__FILE__), 'views')
+    set :public_dir, File.join(File.dirname(__FILE__), 'public')
+    set :layouts_dir, File.join(File.dirname(__FILE__), 'views', 'layouts')
+    set :partials_dir, File.join(File.dirname(__FILE__), 'views', 'partials')
 
     # Mongoid.load!("./mongoid.yml")
     # Set your Google Analytics ID here if you have one:
     # set :google_analytics_id, 'UA-12345678-1'
 
-      #** Configure compass
-      #* @see http://compass-style.org/help/tutorials/integration/
-      #**
-      Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.rb'))
-  end
-
-  def show_404
-    status 404
-    @page_title = '404'
-    erb :'404', :layout => :default,
-                :layout_options => {:views => settings.layouts_dir}
-  end
-
-  def show_500
-    status 500
-    @page_title = '500'
-    erb :'500', :layout => :default,
-                :layout_options => {:views => settings.layouts_dir}
+    #** Configure compass
+    #* @see http://compass-style.org/help/tutorials/integration/
+    #**
+    Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.rb'))
   end
 
   not_found do
-    show_404
+    status 404
+    @page_title = '404'
+    erb :'404', :layout => :default,
+        :layout_options => {:views => settings.layouts_dir}
   end
 
   error do
-    show_500
+    status 500
+    @page_title = '500'
+    erb :'500', :layout => :default,
+        :layout_options => {:views => settings.layouts_dir}
   end
 
   get '/' do
-      @page_title = 'Hello World!'
-      erb :index, :layout => :default,
-                  :layout_options => {:views => settings.layouts_dir}
+    @page_title = 'Hello World!'
+    h
+    erb :index, :layout => :default,
+        :layout_options => {:views => settings.layouts_dir}
   end
 end
